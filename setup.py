a09e734d12c1612cf9beb3aed71682eec3710add
datanooblol <data.noob.lol@gmail.com>
2024-08-13 21:26:37 +0700

    update init

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ezllmaw',
-    version='0.0.13',
+    version='0.0.14',
     packages=find_packages(),
     install_requires=[],
     description='Easy LLM-Agentic Workflow',
