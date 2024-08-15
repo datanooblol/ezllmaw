60fa4bec6453bc53af2389c3d4cd3cd864a041bb
datanooblol <data.noob.lol@gmail.com>
2024-08-16 02:44:43 +0700

    add ollama tool conversion

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ezllmaw',
-    version='0.0.18',
+    version='0.0.19',
     packages=find_packages(),
     install_requires=[],
     description='Easy LLM-Agentic Workflow',
