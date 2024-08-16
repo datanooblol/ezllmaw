ae7f79413b39176d7262c7945164d92bce65557f
datanooblol <data.noob.lol@gmail.com>
2024-08-16 23:04:16 +0700

    debug_print

--- ezllmaw/debug.py
+++ ezllmaw/debug.py
@@ -1,14 +1,14 @@
 import functools
 import ezllmaw as ez
 
-def debug(func, *args, **kwargs):
-    @functools(func)
+def debug_print(func, *args, **kwargs):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if ez.settings.debug == True:
             return func(*args, **kwargs)
     return wrapper
 
-@debug
+@debug_print
 def ez_print(*args, **kwargs)->None:
     """Here is vanilla print."""
     print(*args, **kwargs)
